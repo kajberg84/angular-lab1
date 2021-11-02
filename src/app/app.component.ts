fc1b91513072f6514068cf8332ef0a8182f76988
Kaj Berg <kb223aw@student.lnu.se>
2021-11-02 15:34:50 +0100

    refactor app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'kaj-angular';
+  // links = [
+  //   {path: '/home', icon: 'home', title: 'home'},
+  //   {path: '/aboutme', icon: 'aboutme', title: 'About me'}
+  // ];
+
+  // constructor(){};
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './materialUi/material.module';
+import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutmeComponent
+    AboutmeComponent,
+    HomeComponent,
+    Page1Component,
   ],
   imports: [
     BrowserModule,
